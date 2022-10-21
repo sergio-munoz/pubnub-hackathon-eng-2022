b61801ea33cde99323721332a56db6b7cd910cbd
Sergio M <110481361+sergio-munoz@users.noreply.github.com>
2022-10-20 20:35:20 -0400

    First commit. Nuxt3 typescript using nuxi.

---
+++ pubnub-python-tools-frontend/app.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>PubNub Python Tools</h1>
+    <p>Quickly interact with PubNub using Python.</p>
+    <div class="options-container">
+      <div class="option-container">
+        <h4>1) Choose a tool</h4>
+        <div class="option-buttons"> 
+          <button>Subscribe</button>
+          <button>Publish</button>
+          <button>Herenow</button>
+        </div>
+      </div>
+      <div class="option-container">
+        <h4>2) Add Presence</h4>
+        <div class="option-buttons"> 
+          <button>Yes</button>
+          <button>No</button>
+        </div>
+      </div>
+      <div class="option-container">
+        <h4>3) Choose your channel</h4>
+        <div class="option-buttons"> 
+          <button>test.channel</button>
+          <button>random_channel</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
